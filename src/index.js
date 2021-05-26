a2ed86f0de8934357d6f0035385435ea32f8f16d
smsguy927 <smsguy927@gmail.com>
2021-05-26 10:29:46 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+function addTask(event) {
+  event.preventDefault();
+  let input = this.children[0];
+
+  let taskText = input.value;
+
+  input.value = "";
+  let taskList = document.querySelector("#tasks");
+  let newTask = document.createElement("li");
+  newTask.innerText = taskText;
+  taskList.appendChild(newTask);
+}
+let addTaskSection = document.querySelector("#create-task-form");
+
+addTaskSection.addEventListener("submit", addTask);
